76dfd8e96fbe073fdffc4209d0ecaf919bce5cc8
ctolmsted <prunes03smugger@icloud.com>
2023-01-12 14:27:02 -0500

    made phil appear and disappear as needed

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+
+    chrome.tabs.sendMessage(tab.id, {type: "runExtension"});
+
+  }
+})

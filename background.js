13753d1a18689ddef2c9d29bbb4b61c33a07d56f
Melody Duan <melodyduany@Melodys-Laptop.local>
2023-01-12 14:37:51 -0500

    background.js slacked code updated cause git is stupid

--- background.js
+++ background.js
@@ -1,12 +1,7 @@
-chrome.tabs.onUpdated.addListener(
-  function(tabId, changeInfo, tab) {
-    console.log("we're in background.js!")
-    console.log(tabID, changeInfo, tab)
-    if (changeInfo.url) {
-      chrome.tabs.sendMessage( tabId, {
-        message: 'hello!',
-        url: changeInfo.url
-      })
-    }
+chrome.tabs.onUpdated.addListener( function (tabId, changeInfo, tab) {
+  if (changeInfo.status == 'complete') {
+
+    chrome.tabs.sendMessage(tab.id, {type: "runExtension"});
+
   }
-);
+})
